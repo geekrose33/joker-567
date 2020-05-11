d88ba1f172a4682921b65b11395e0865778ad493
joker-567 <62861464+joker-567@users.noreply.github.com>
2020-05-11 19:00:26 +0800

    Day_05
    构造方法

---
+++ day_05/src/com/company/TextStudent.java
@@ -0,0 +1,15 @@
+package com.company;
+
+/**
+ * @author Joker_Dong
+ * @date 2020-5-11 18:32
+ */
+
+public class TextStudent {
+    public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println(student);
+        Student student1 = new Student("金豆豆",18);
+        System.out.println(student1);
+    }
+}
