e8536040bb2a9a4898a6d387d0acbfb414b9ddcb
joker-567 <62861464+joker-567@users.noreply.github.com>
2020-05-14 18:54:41 +0800

    ArrayList作为返回值

--- day_05/src/com/company/Test01_ArrayList.java
+++ day_05/src/com/company/Test01_ArrayList.java
@@ -1,5 +1,9 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 /**
  * @author Joker_Dong
  * @date 2020-5-14 18:00
@@ -7,6 +11,42 @@ package com.company;
 
 public class Test01_ArrayList {
     public static void main(String[] args) {
-        // 生成10个1到33之间的随机整数 添加到集合里 并且遍历
+        // 生成10个1到33之间的随机整数 添加到集合里 并且遍历输出
+        Random random = new Random();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int j = 0; j < 10; j++) {
+            int i = random.nextInt(34);
+            list.add(i);
+
+        }
+        System.out.println("随机生成数字个数： " + list.size());
+        /*for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+        */
+        printArrayList(list);
     }
+
+    // 自定义方法打印List
+    public static void printArrayList(ArrayList<Integer> list){
+        // 拼接左括号
+
+        System.out.print(" { ");
+        // 遍历集合
+        for (int i = 0; i < list.size(); i++) {
+            // 获取元素
+            Integer num = list.get(i);
+            if (i != list.size()-1){
+                System.out.print(num + " ,");
+            }else {
+                System.out.print(num);
+
+            }
+        }
+
+        // 拼接右括号
+        System.out.print(" } ");
+
+    }
+
 }
---
+++ day_05/src/com/company/Test02_ArrayList.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+/**
+ * @author Joker_Dong
+ * @date 2020-5-14 18:27
+ */
+
+public class Test02_ArrayList {
+    public static void main(String[] args) {
+        Random r = new Random();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            int num = r.nextInt(1001);
+            list.add(num);
+        }
+
+        // 调用一个方法 筛选出List 里面的偶数组成的新集合
+        for (int i = 0; i < list.size(); i++) {
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+        System.out.println("---------------------------");
+        System.out.println(printOddList(list));
+        System.out.println("-------------");
+        ArrayList<Integer> list1 = printOddList(list);
+        for (int i = 0; i < list1.size(); i++) {
+            System.out.println(list1.get(i));
+        }
+
+
+    }
+
+    public static ArrayList<Integer> printOddList(ArrayList<Integer> list){
+        // 创建一个小集合 存储偶数
+        ArrayList<Integer> smallList = new ArrayList<>();
+        // 遍历传入的list
+        for (int i = 0; i < list.size(); i++) {
+            // 获取每一个元素
+            Integer num = list.get(i);
+            if (num%2 == 0){
+                smallList.add(num);
+            }
+        }
+        return smallList;
+
+    }
+
+}
