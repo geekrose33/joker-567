20fbf59b326f4f09c8d00903ce478a8f08bae691
joker-567 <62861464+joker-567@users.noreply.github.com>
2020-05-16 20:46:36 +0800

    方法练习

---
+++ HomeExample/src/com/company/Shift.java
@@ -0,0 +1,21 @@
+package com.company;
+
+/**
+ * @author Joker_Dong
+ * @date 2020-5-16 19:52
+ */
+
+public class Shift {
+    public static void main(String[] args) {
+        int i = 1 << 10;
+        System.out.println(i);
+        int j = 1 << 5;
+        System.out.println(j);
+        System.out.println(getShift(5));
+    }
+    public static int getShift(int num){
+        int j = num << 10;
+        return j;
+
+    }
+}
