b4c182811c47010d68ab7a793ef72f12a9cbfc84
joker-567 <62861464+joker-567@users.noreply.github.com>
2020-06-02 13:22:06 +0800

    补交git

---
+++ HomeExam_0523/src/com/company/StringEqualsExam_02.java
@@ -0,0 +1,35 @@
+package com.company;
+
+/**
+ * @author Joker_Dong
+ * @date 2020-5-23 8:41
+ */
+
+public class StringEqualsExam_02 {
+    private char []value;
+    public StringEqualsExam_02(char[] value){this.value = value;}
+    public boolean equall(Object object1){
+        if (this == object1){return true;}
+        if (object1 instanceof StringEqualsExam_02){
+            StringEqualsExam_02 object2 = (StringEqualsExam_02)object1;
+            int n = value.length;
+            if (n == object2.value.length){
+                int i = 0;
+                char[] v1 = value;
+                char[] v2 = object2.value;
+                while (n-- != 0){
+                    if (v1[i] == v2[i])
+                        return true;
+                    i++;
+                }
+
+            }
+            return false;
+        }
+
+        return false;
+    }
+}
+
+
+
---
+++ HomeExam_0523/src/com/company/StringExam_01.java
@@ -0,0 +1,30 @@
+package com.company;
+
+/**
+ * @author Joker_Dong
+ * @date 2020-5-23 8:23
+ */
+
+public class StringExam_01 {
+    public static void main(String[] args) {
+/*
+        String str1 = new String("abc");
+        System.out.println(str1);
+        String str2 = "abc";
+        String str3 = "ABC";
+        // String 类 的equals、equalsIgnore方法
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.equals(str3));
+        System.out.println("------------------------------------");
+        System.out.println(str1.equalsIgnoreCase(str3));
+*/
+
+        char[] data = {'a','b','c'};
+        System.out.println(data.clone() != data);
+        System.out.println(data.clone());
+
+
+
+
+    }
+}
