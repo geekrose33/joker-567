034214d122f2be2356910605e97cf3d32d46e7e2
joker-567 <62861464+joker-567@users.noreply.github.com>
2020-06-14 15:17:00 +0800

    匿名内部类

---
+++ day_09/src/com/company/InnerDemoTest3.java
@@ -0,0 +1,29 @@
+package com.company;
+
+/**
+ * @author Joker_Dong
+ * @date 2020-6-14 15:06
+ */
+
+public class InnerDemoTest3 {
+
+    public static void main(String[] args) {
+        /*
+        * 1. 等号右边 定义并创建该接口的子类对象
+        * 2. 等号左边 是多态 接口类型引用指向子类对象
+        * */
+
+        FlyAble f = new FlyAble(){
+            @Override
+            public void fly() {
+                System.out.println("起飞飞飞飞飞飞飞");
+            }
+        }; // 这是一句话
+
+        showFly(f);
+    }
+    // 将带有匿名内部类作为方法的参数进行传递
+    public static void showFly(FlyAble f){
+        f.fly();
+    }
+}
