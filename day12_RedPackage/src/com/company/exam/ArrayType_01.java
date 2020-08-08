7f6c419af71110c6a99ed88b97d7f833d52f8454
joker-567 <62861464+joker-567@users.noreply.github.com>
2020-08-08 20:35:28 +0800

    11

--- day12_RedPackage/src/com/company/exam/ArrayType_01.java
+++ day12_RedPackage/src/com/company/exam/ArrayType_01.java
@@ -8,6 +8,26 @@ package com.company.exam;
 public class ArrayType_01 {
     public static void main(String[] args) {
         // 数组Array
+        int []arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+
+        }
+        // System类的arraycopy方法
+        int []arr1 = new int[3];
+        System.arraycopy(arr,0,arr1,0,3);
+        System.out.println(arr); // arr是数组的引用（地址值）不能输出实际的数组内的值
+
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.println(arr1[i]);
+        }
+
+        
+
+
 
     }
 }
